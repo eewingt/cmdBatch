86303750b206d996639d17a4fa4b27f470e8fa1e
eewingt <89181023+eewingt@users.noreply.github.com>
2023-09-20 11:11:37 +0800

    Update RenameFiles.bat

--- RenameFiles.bat
+++ RenameFiles.bat
@@ -1,7 +1,11 @@
 cd C:\logs
-ren Pokemon.txt Pokemon2.txt
 
-If exist C:\logs\Pokemon2.txt (
+SET /P var1=
+echo "Will be renaming file to "%var1%
+
+ren Pokemon.txt %var1%.txt
+
+If exist C:\logs\%var1%.txt (
 msg "%username%" Rename successful
 )
 
